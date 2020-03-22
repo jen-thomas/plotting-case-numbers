f44bbc977b24efb0ffd0632830244279a1b75026
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-03-22 16:03:50 +0000

    Added axes labels.

--- plot-case-numbers.py
+++ plot-case-numbers.py
@@ -2,8 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import colors
 
-data = [['2020-03-01', 'Spain', 10, 0], ['2020-03-02', 'Spain', 25, 2], ['2020-03-03', 'Spain', 40, 4],
-        ['2020-03-01', 'Italy', 20, 0], ['2020-03-02', 'Italy', 50, 5], ['2020-03-03', 'Italy', 70, 10],
+data = [['2020-03-01', 'Spain', 150, 0], ['2020-03-02', 'Spain', 252, 2], ['2020-03-03', 'Spain', 400, 4],
+        ['2020-03-01', 'Italy', 200, 0], ['2020-03-02', 'Italy', 250, 5], ['2020-03-03', 'Italy', 470, 10],
         ]
 
 # Create the pandas DataFrame 
@@ -15,7 +15,7 @@ print(df)
 
 
 # Define colours of grouped points
-colors = {'Spain':'red', 'Italy':'blue'}
+colours = {'Spain':'red', 'Italy':'blue'}
 
 # Create axes for plot
 fig, ax = plt.subplots(figsize=(8,6))
@@ -23,6 +23,8 @@ fig, ax = plt.subplots(figsize=(8,6))
 # Group the data by country and plot the number of cases per day
 grouped = df.groupby('Country')
 for key, group in grouped:
-    group.plot(ax=ax, kind='line', x='Date', y='Number of cases', label=key, color=colors[key])
+    group.plot(ax=ax, kind='line', x='Date', y='Number of cases', label=key, color=colours[key])
 plt.show()
+ax.set_xlabel('Date')
+ax.set_ylabel('Number of cases')
 
