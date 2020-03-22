6d95beffaa21cac4230951eb4eb08c3ffc5c081f
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-03-22 18:49:51 +0000

    Convert number of cases and deaths to numerical values.

--- plot-case-numbers.py
+++ plot-case-numbers.py
@@ -10,6 +10,8 @@ def create_test_dataframe():
     # Create the pandas DataFrame
     df = pd.DataFrame(data, columns=['Date', 'Country', 'Number of cases', 'Number of deaths'])
     df['Date'] = pd.to_datetime(df['Date'])
+    df['Number of cases'] = pd.to_numeric(df['Number of cases'])
+    df['Number of deaths'] = pd.to_numeric(df['Number of deaths'])
 
     # print dataframe.
     print(df)
@@ -21,6 +23,8 @@ def put_data_list_into_dataframe(data_list):
 
     df = pd.DataFrame(data_list, columns=['Country', 'Date', 'Number of cases', 'Number of deaths'])
     df['Date'] = pd.to_datetime(df['Date'])
+    df['Number of cases'] = pd.to_numeric(df['Number of cases'])
+    df['Number of deaths'] = pd.to_numeric(df['Number of deaths'])
 
     return df
 
@@ -41,16 +45,19 @@ def plot_dataframe_group_country(df):
     grouped = df.groupby('Country')
     for key, group in grouped:
         group.plot(ax=ax, kind='line', x='Date', y='Number of cases', label=key, color=colours[key])
-    plt.show()
+
     ax.set_xlabel('Date')
     ax.set_ylabel('Number of cases')
 
+    plt.show()
+
 
 if __name__ == '__main__':
-    data_list = [['Spain', '2020-03-01', 150, 0], ['Spain', '2020-03-02', 252, 2], ['Spain', '2020-03-03', 400, 4], \
-            ['Italy', '2020-03-01', 200, 0], ['Italy', '2020-03-02', 250, 5], ['Italy', '2020-03-03', 470, 10],
-            ]
-    df = put_data_list_into_dataframe(data_list)
 
+    data_list = get_data.get_data_for_countries(['Spain', 'Italy'])
+    #data_list = [['Spain', '2020-03-01', 150, 0], ['Spain', '2020-03-02', 252, 2], ['Spain', '2020-03-03', 400, 4], \
+    #        ['Italy', '2020-03-01', 200, 0], ['Italy', '2020-03-02', 250, 5], ['Italy', '2020-03-03', 470, 10],
+    #        ]
+    df = put_data_list_into_dataframe(data_list)
 
     plot_dataframe_group_country(df)
