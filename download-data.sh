a8df0155c715614eec78ae085e6412b61d35ecbb
Carles Pina i Estany <carles@pina.cat>
2020-03-22 19:28:02 +0100

    Adds file to download the data and to read it

--- download-data.sh
+++ download-data.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# Data from: https://github.com/CSSEGISandData/COVID-19
+
 wget "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv" -O data/time_series_19-covid-Confirmed.csv
-wget "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv" -O data/time_series_19-covid-Deaths.csv
+wget "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv" -O data/time_series_19-covid-Deaths.csv
---
+++ get_data.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+import csv
+
+
+def countries_population():
+    # From Wikipedia
+    return {'Spain': 46_733_038,
+            'UK': 67_454_757,
+            'Switzerland': 8_570_146,
+            'Italy': 60_317_546,
+            'US': 328_239_523,
+            'Germany': 83_149_300
+            }
+
+
+def get_data(file_path, country):
+    output = []
+    with open(file_path) as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+
+        for country_information in csv_reader:
+            if country_information['Country/Region'] == country:
+                output = process_country(country_information)
+
+    return output
+
+
+def process_country(country_information):
+    result = {}
+
+    reading = False
+    for key, items in country_information.items():
+        if key == '1/22/20':
+            reading = True
+
+        if reading:
+            result[key] = items
+
+    return result
+
+
+def merge(confirmed, deaths):
+    output = []
+    for date in confirmed:
+        output.append([date, confirmed[date], deaths[date]])
+
+    return output
+
+
+def print_confirmed_deaths_rate(confirmed_and_deaths):
+    previous_confirmed = previous_deaths = 0
+    for item in confirmed_and_deaths:
+        date, confirmed, deaths = item
+        confirmed = int(confirmed)
+        deaths = int(deaths)
+
+        if previous_confirmed == 0:
+            rate_confirmed = None
+        else:
+            rate_confirmed = f'{confirmed / previous_confirmed:.2f}'
+
+        if previous_deaths == 0:
+            rate_deaths = None
+        else:
+            rate_deaths = f'{deaths / previous_deaths:.2f}'
+
+        print(f'{date}\t{confirmed}\t{deaths}\t{rate_confirmed}\t{rate_deaths}')
+
+        previous_confirmed = confirmed
+        previous_deaths = deaths
+
+
+if __name__ == '__main__':
+    confirmed = get_data('data/time_series_19-covid-Confirmed.csv', 'Spain')
+    deaths = get_data('data/time_series_19-covid-Deaths.csv', 'Spain')
+
+    confirmed_and_deaths = merge(confirmed, deaths)
+    print(confirmed_and_deaths)
+
+    print_confirmed_deaths_rate(confirmed_and_deaths)
